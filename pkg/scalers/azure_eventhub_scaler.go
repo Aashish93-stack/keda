bb8201a412e79ca588f70aeff68cb38d4e78d569
Aashish93-stack <106277711+Aashish93-stack@users.noreply.github.com>
2022-10-31 16:52:54 -0700

    Fix log message for checkpoint Identity Id
    Signed-off-by: Aashish93-stack <106277711+Aashish93-stack@users.noreply.github.com>

--- pkg/scalers/azure_eventhub_scaler.go
+++ pkg/scalers/azure_eventhub_scaler.go
@@ -213,7 +213,7 @@ func parseAzureEventHubAuthenticationMetadata(logger logr.Logger, config *Scaler
 		if val, ok := config.TriggerMetadata["CheckpointIdentity"]; ok {
 			meta.eventHubInfo.CheckpointIdentityId = val
 		} else {
-			logger.Info("no 'CheckpointIdentity' supplied to enable identity based authentication to Blob Storage. Using Pod Identity instead or connection string instead if no 'storageAccountName' supplied")
+			logger.Info("no 'CheckpointIdentityId' supplied to enable identity based authentication to Blob Storage. Using Pod Identity or connection string instead if no 'storageAccountName' supplied")
 		}
 
 		if len(meta.eventHubInfo.StorageAccountName) != 0 {
